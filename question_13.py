e9c6bcf725835017b9285d776c9587c5ff636c93
Chandanakerelly <166921211+Chandanakerelly@users.noreply.github.com>
2024-07-23 19:32:50 +0530

    DAy-3

---
+++ question_13.py
@@ -0,0 +1,50 @@
+'''
+find the sum of element that is present in k+n index
+k is given by user
+k=3
+n=2
+3 6 8 4 61 2
+0p:2
+k=3
+n=4
+80 70 54 36 72
+
+
+'''
+
+# li=list(map(int,input().split(" ")))
+# k=int(input())
+# n=int(input())
+# if len[li]>k+n:
+#     print("error")
+# else:
+#     for i in range(len[li]):
+#         print(li[k+n])
+#         break
+
+
+# li=list(map(int,input().split(" ")))
+# k=int(input())
+# n=int(input())
+# for i in range(0,len(li)):
+#     print(li[k+n],end=" ")
+    
+'''
+print the element in a particular index in cyclic printing
+
+
+
+'''
+# li=list(map(int,input().split()))
+# k=int(input())
+# idx=k%len(li)
+# print(li[idx])
+
+
+
+
+
+
+
+
+
---
+++ question_14.py
@@ -0,0 +1,28 @@
+'''
+find the max element in the given list
+
+
+
+
+'''
+# li=list(map(int,input().split()))
+# max=li[0]
+# for i in range(len(li)):
+#     if(li[i]>max):
+#         max=li[i]
+# print(max)
+
+
+# min element
+# li=list(map(int,input().split()))
+# max=li[0]
+# for i in range(len(li)):
+#     if(li[i]<max):
+#         max=li[i]
+# print(max)
+
+# replace the elements in an array with the average of max and elements
+
+
+
+a=int(input())
